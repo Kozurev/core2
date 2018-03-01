5f057e66202a9450d3183abb761565a393830d27
egor <creative27016@gmail.com>
2018-03-01 23:46:03 +0300

    Добавлены новые свойства и методы
    Добавлена возможность подгружать дочерние структуры и эленты для этих структур

--- model/structure/controller.php
+++ model/structure/controller.php
@@ -23,6 +23,36 @@ class Structure_Controller extends Structure_Controller_Model
 		return $aoItems;
 	}
 
+    /**
+     * Получение списка дочерних структур (только первого уровня или всего древа)
+     * @param bool $bAllTree:
+     *      false - возвращаются дочерние структуры первого уровня
+     *      true - возвращается всё древо дочерних структур
+     * @return array
+     */
+    private function getChildren(bool $bWithItems = false)
+    {
+        $aoChildren = Core::factory("Structure")
+            ->where("parent_id", "=", $this->id)
+            ->findAll();
+
+        if(count($aoChildren) == 0) return array();
+
+        $aoResult = $aoChildren;
+
+        foreach($aoChildren as $oStructure)
+        {
+            $aoResult = array_merge($aoResult, $oStructure->getChildren());
+        }
+
+        if($bWithItems === true)
+        {
+            $aoResult = array_merge($aoResult, $this->getItemsForStructures($aoResult));
+        }
+
+        return $aoResult;
+    }
+
 
 	public function findAll()
 	{
@@ -31,11 +61,32 @@ class Structure_Controller extends Structure_Controller_Model
 		if($this->items()) 
 		{
 			$outputData = array_merge($outputData, $this->getItemsForStructures($outputData));
-		}		
-		if($this->properties())	
-		{
-			$outputData = array_merge($outputData, $this->getPropertiesValuesForItems($outputData));
 		}
+
+		if($this->children())
+        {
+            $aoChildren = array();
+
+            foreach ($outputData as $item)
+            {
+                if(get_class($item) == "Structure")
+                {
+                    $aoChildren = array_merge($aoChildren, $item->getChildren());
+                }
+            }
+
+            if($this->childrenWithItems())
+            {
+                $aoChildren = array_merge($aoChildren, $this->getItemsForStructures($aoChildren));
+            }
+
+            $outputData = array_merge($outputData, $aoChildren);
+        }
+
+        if($this->properties())
+        {
+            $outputData = array_merge($outputData, $this->getPropertiesValuesForItems($outputData));
+        }
 
 		return $outputData;
 	}
