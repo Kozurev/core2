e86011138ed11c6214f3c057547ac98906ee4ded
egor <creative27016@gmail.com>
2018-03-01 22:52:07 +0300

    Изменение формата подгрузки списка
    1. Теперь для каждого списка отдельный метод, который подгружает элементы.
    2. Доработана подгрузка элементов для списка "Родительский раздел": теперь в качестве родительского раздела структуры не может быть дочерняя структура

--- model/admin/form.php
+++ model/admin/form.php
@@ -1,5 +1,6 @@
 <?php 
 
+
 class Admin_Form extends Admin_Form_Model 
 {
 
@@ -8,14 +9,56 @@ class Admin_Form extends Admin_Form_Model
 		
 	}
 
+    /**
+     * @param $aParams - массив параметров формирования страницы
+     * @return void
+     * Поиск и добавление элементов для списка "Родительский раздел"
+     * При редактировании существующего элемента отсеиваются всё древо дочерних структур
+     * При создании нового элемента добавляются все структуры
+     */
+    public function getListStructures($aParams)
+    {
+        $aoStructures = Core::factory("Structure")->findAll();
 
-	public function getList()
-	{
-		$aoListItems = Core::factory((string)$this->list_name)
-			->findAll();
+        isset($aParams["model_id"])
+            ?	$sObjectId = (string)$aParams["model_id"]
+            :	$sObjectId = "0";
 
-		$this->addEntities($aoListItems, "item");
-	}
+        isset($aParams["parent_id"])
+            ?	$this->value = (string)$aParams["parent_id"]
+            :	$this->value = "0";
+
+        //Если редактируется существующая структура
+        if($sObjectId != "0" && $this->model_id == 1)
+        {
+            $oCurentStructure = Core::factory("Structure", $sObjectId);
+
+            foreach($aoStructures as $oStructure)
+            {
+                if(!$oCurentStructure->isChild($oStructure) && $oCurentStructure->getId() != $oStructure->getId())
+                {
+                    $this->addEntity($oStructure, "item");
+                }
+
+            }
+        }
+        //Если создается новая структура
+        else
+        {
+            $this->addEntities($aoStructures, "item");
+        }
+    }
+
+
+    /**
+     * Поиск элементов для списка "Макет"
+     * @param $aParams
+     * @return void
+     */
+    public function getListTemplates($aParams)
+    {
+        $this->addEntities(Core::factory("Page_Template")->findAll(), "item");
+    }
 
 
 }
