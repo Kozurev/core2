5d0ee47a47e09d1bef8072965ff7e6df9cdf0c00
egor <creative27016@gmail.com>
2018-03-04 02:18:34 +0300

    Файл, содержащий шаблоны часто повторяющихся строк

---
+++ config/messages/ru/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    "UNDEFIND_STRING_NAME" => "Не удалось найти значение строки $sMessageName <br>",
+    "ERROR_404" => "ERROR 404",
+    "TOO_LARGE_VALUE" => "Длина свйоства $aMessageParams[0] объекта $aMessageParams[1] не должна превышать $aMessageParams[2] символов. <br>",
+    "INVALID_TYPE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно иметь тип $aMessageParams[2]. <br>",
+    "UNSIGNED_VALUE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно быть больше либо равным нулю. <br>",
+);
