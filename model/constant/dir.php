2b2cb76938f3593409144b8435127babf36a6dcc
egor <creative27016@gmail.com>
2018-03-04 22:55:46 +0300

    Добавлены дирректории констант

---
+++ model/constant/dir.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Egor
+ * Date: 04.03.2018
+ * Time: 16:55
+ */
+
+class Constant_Dir extends Constant_Dir_Model
+{
+
+    public function __construct()
+    {
+    }
+
+}
