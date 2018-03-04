b3e0d32c596e8b8abc4255bf6205ee052f06e8d0
egor <creative27016@gmail.com>
2018-03-04 22:55:31 +0300

    Теперь константа имеет тип
    Добавлены типы значений констант (int, float, bool)

--- model/constant.php
+++ model/constant.php
@@ -12,15 +12,41 @@ class Constant extends Constant_Model
 	{
 		$aConstants = Core::factory('Constant');
 		$aConstants = $aConstants->queryBuilder()
+            ->where("active", "=", "1")
 			->findAll();
 
 		if($aConstants === false) return;
 
 		foreach ($aConstants as $const) 
-		{
-			define($const->title(), $const->value());				
-		}
+        {
+            $sConstName = $const->name();
+            $val = $const->value();
+            $sValueType = Core::factory("Constant_Type", $const->valueType())->title();
 
+            switch($sValueType)
+            {
+                case "bool":
+                    {
+                        define($sConstName, boolval($val));
+                        break;
+                    }
+                case "int":
+                    {
+                        define($sConstName, intval($val));
+                        break;
+                    }
+                case "float":
+                    {
+                        define($sConstName, floatval($val));
+                        break;
+                    }
+
+                default:
+                    {
+                        define($sConstName, strval($val));
+                    }
+            }
+		}
 	}
 
 
@@ -28,28 +54,28 @@ class Constant extends Constant_Model
 	*	Метод включает в себя проверку на существование константы с таким же именем
 	*	@return self
 	*/
-	public function save()
-	{
-		try
-		{
-			$const = Core::factory("Constant");
-			$result = $const->queryBuilder()
-				->where("title", "=", $this->title)
-				->find();
-
-			if(!$result)
-			{
-				parent::save();
-				return $this;
-			}
-
-			throw new Exception("Константа с таким названием уже существует");
-		}
-		catch(Exception $e)
-		{
-			echo "<br>".$e->getMessage();
-		}
-	}
+//	public function save()
+//	{
+//		try
+//		{
+//			$const = Core::factory("Constant");
+//			$result = $const->queryBuilder()
+//				->where("title", "=", $this->title)
+//				->find();
+//
+//			if(!$result)
+//			{
+//				parent::save();
+//				return $this;
+//			}
+//
+//			throw new Exception("Константа с таким названием уже существует");
+//		}
+//		catch(Exception $e)
+//		{
+//			echo "<br>".$e->getMessage();
+//		}
+//	}
 
 
 
