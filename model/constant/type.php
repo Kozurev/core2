6ab739fe13a5fa801c44b008281297f445262b02
egor <creative27016@gmail.com>
2018-03-04 22:43:02 +0300

    idea

---
+++ model/constant/type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Egor
+ * Date: 04.03.2018
+ * Time: 21:30
+ */
+
+class Constant_Type extends Constant_Type_Model
+{
+    public function __construct()
+    {
+    }
+}
