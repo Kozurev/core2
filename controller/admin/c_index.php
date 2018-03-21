30c535f4d60771069f4963fedd80c5b6e96f3184
Kozurev <creative27016@gmail.com>
2018-03-21 17:05:16 +0300

    Summary

--- controller/admin/c_index.php
+++ controller/admin/c_index.php
@@ -1,5 +1,10 @@
 <?php
 
+$oUser = Core::factory("User");
+$aAccessParams = array("superuser" => 1, "groups" => array(1));
+if($oUser::checkUserAccess($aAccessParams) != true)
+    die(Core::getMessage("ACCESS_DENIED", array()));
+
 if(isset($_GET["menuTab"]) && isset($_GET["menuAction"]))
 {
 	$tabName = $_GET["menuTab"];
