6250a57078b5b58520a2086a6a4e22067fb56527
Kozurev <creative27016@gmail.com>
2018-03-21 17:04:09 +0300

    Новое сообщение

--- config/messages/ru/messages.php
+++ config/messages/ru/messages.php
@@ -6,4 +6,5 @@ return array(
     "TOO_LARGE_VALUE" => "Длина свйоства $aMessageParams[0] объекта $aMessageParams[1] не должна превышать $aMessageParams[2] символов. <br>",
     "INVALID_TYPE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно иметь тип $aMessageParams[2]. <br>",
     "UNSIGNED_VALUE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно быть больше либо равным нулю. <br>",
+    "ACCESS_DENIED" => "Доступ к данной странице запрещен",
 );
