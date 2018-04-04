463475ae414acc0808ee08d62f88bb81890f4ab1
egor <creative27016@gmail.com>
2018-04-04 23:53:55 +0300

    Доработки

--- model/admin/menu/form.php
+++ model/admin/menu/form.php
@@ -1,5 +1,5 @@
 <?php
-
+//Формы редактирования
 class Admin_Menu_Form
 {
 	public function __construct(){}
@@ -11,23 +11,31 @@ class Admin_Menu_Form
 
 		if(isset($aParams["parent_id"]))	$parentId = $aParams["parent_id"];
 		else 								$parentId = 0;
+
+		$oParentId = Core::factory("Core_Entity")
+            ->name("parent_id")
+            ->value($parentId);
 		
 		if(!$parentId)		$title = "Список моделей";
 		elseif($parentId)	$title = Core::factory("Admin_Form_Modelname", $parentId)->model_name();
-		
+
 		if($parentId)	
 		{
 			$aoData = Core::factory("Admin_Form")
+                ->orderBy("sorting")
 				->where("model_id", "=", $parentId)
 				->findAll();
 		}
 		else 
 		{
 			$aoData = Core::factory("Admin_Form_Modelname")
+                ->orderBy("model_sorting")
+                ->where("indexing", "=", "1")
 				->findAll();
 		}
 
 		Core::factory("Core_Entity")
+            ->addEntity($oParentId)
 			->addEntity(
 				Core::factory("Core_Entity")
 					->name("title")
@@ -36,7 +44,6 @@ class Admin_Menu_Form
 			->addEntities($aoData)
 			->xsl($usingXslLink)
 			->show();
-
 	}
 
 
