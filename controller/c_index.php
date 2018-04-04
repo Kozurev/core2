91bd5142be6d306ec59771931e8feb242749ffbd
egor <creative27016@gmail.com>
2018-04-04 23:51:13 +0300

    Summary

--- controller/c_index.php
+++ controller/c_index.php
@@ -1,22 +1,3 @@
 <?php
 
-//    $musdb = new PDO("mysql:host=localhost;dbname=musadm", "root", "");
-//
-//    $data = $musdb->query("SELECT * from role_assignment where userid > 40");
-//    $data->setFetchMode(PDO::FETCH_OBJ);
-//    $users = $data->fetchAll();
-//
-//    echo "<pre>";
-//    print_r($users);
-//
-//    foreach ($users as $user)
-//    {
-//
-//    }
-
-echo "<pre>";
-
-$arr = array("first" => 1, "second" => 2, "third" => 3);
-
-print_r($arr);
-var_dump(Core_Array::unsetValue($arr, "second"));
+echo Core::factory("Admin_Form")->getCount();
