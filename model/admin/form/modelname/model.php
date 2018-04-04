a2e6ed6f8e91c6f552caf2872bb93f8c3aaac5f8
egor <creative27016@gmail.com>
2018-04-04 23:52:56 +0300

    Добавлены новые свойства

--- model/admin/form/modelname/model.php
+++ model/admin/form/modelname/model.php
@@ -4,7 +4,9 @@ class Admin_Form_Modelname_Model extends Core_Entity
 {
 	protected $id;
 	protected $model_name;
-
+    protected $model_title;
+    protected $model_sorting;
+    protected $indexing = 1;
 
 	public function getId() 
 	{
@@ -12,7 +14,7 @@ class Admin_Form_Modelname_Model extends Core_Entity
 	}
 
 
-	public function model_name(string $val = null)
+	public function model_name($val = null)
 	{
 		if(is_null($val))		return $this->model_name;
 		if(strlen($val) > 100)	die('Значение свойства "model_name" не должно превышать 100 символов.');
@@ -22,4 +24,28 @@ class Admin_Form_Modelname_Model extends Core_Entity
 	}
 
 
+	public function model_title($val = null)
+    {
+        if(is_null($val))   return $this->model_title;
+        if(strlen($val) > 255)	die('Значение свойства "model_title" не должно превышать 255 символов.');
+        $this->model_title = $val;
+        return $this;
+    }
+
+
+    public function model_sorting($val = null)
+    {
+        if(is_null($val))   return $this->model_sorting;
+        $this->model_sorting = intval($val);
+        return $this;
+    }
+
+
+    public function indexing($val = null)
+    {
+        if(is_null($val))   return $this->indexing;
+        if($val == true)    $this->indexing = 1;
+        else $this->indexing = 0;
+        return $this;
+    }
 }
