d9cb19dc5fe2d1c816449e5192650d37b297ee25
egor <creative27016@gmail.com>
2018-04-04 23:50:59 +0300

    Добавлено новое сообщение

--- config/messages/ru/messages.php
+++ config/messages/ru/messages.php
@@ -7,4 +7,5 @@ return array(
     "INVALID_TYPE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно иметь тип $aMessageParams[2]. <br>",
     "UNSIGNED_VALUE" => "Свойство $aMessageParams[0] объекта $aMessageParams[1] должно быть больше либо равным нулю. <br>",
     "ACCESS_DENIED" => "Доступ к данной странице запрещен",
+    "NOT_NULL" => "Свойство $aMessageParams[0] модели $aMessageParams[1] не должно быть пустым.",
 );
