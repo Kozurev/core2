8150aefc420ea987e6610d324ab632d0c7050922
egor <creative27016@gmail.com>
2018-04-09 11:41:10 +0300

    Добавлен переопределенный метод delete
    После удаления значения доп. свойства типа список удаляются для всех объектов, которым это значнеие принадлежит

--- model/property/list/values.php
+++ model/property/list/values.php
@@ -2,5 +2,16 @@
 
 class Property_List_Values extends Property_List_Values_Model
 {
-	
+	public function delete($obj = null)
+    {
+        $aoPropertyLists = Core::factory("Property_List")
+            ->where("property_id", "=", $this->property_id)
+            ->where("value_id", "=", $this->id)
+            ->findAll();
+
+        if($aoPropertyLists != false)
+            foreach ($aoPropertyLists as $val)  $val->delete();
+
+        parent::delete();
+    }
 }
