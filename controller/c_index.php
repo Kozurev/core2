654f11551332d3d8cc2754d79a1fd883917a934b
Kozurev <creative27016@gmail.com>
2018-04-13 16:05:11 +0300

    Summary

--- controller/c_index.php
+++ controller/c_index.php
@@ -1,1 +1,9 @@
 <?php
+
+
+$oProperty = Core::factory("Property");
+$oUserGroup = Core::factory("User_Group", 5);
+$aoGroupProperties = $oProperty->getPropertiesList($oUserGroup);
+
+echo "<pre>";
+print_r($aoGroupProperties);
