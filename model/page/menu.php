d9441517d46cb6662ebdcbdda05f320f308ce015
egor <creative27016@gmail.com>
2018-04-16 02:03:17 +0300

    Объект меню

---
+++ model/page/menu.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Egor
+ * Date: 15.04.2018
+ * Time: 17:05
+ */
+
+class Page_Menu extends Page_Menu_Model
+{
+    public function __construct()
+    {
+    }
+
+
+    public function showMenu1()
+    {
+        //$aoStructures
+    }
+
+}
