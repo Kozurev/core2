ecb2cd8e109f7594ded118308b43de91efc66728
egor <creative27016@gmail.com>
2018-04-16 02:01:02 +0300

    Добавлена проверка авторизации

--- controller/musadm/user/s_index.php
+++ controller/musadm/user/s_index.php
@@ -6,6 +6,27 @@
  * Time: 22:16
  */
 
+if(!$this->oStructureItem)
+{
+    $this->error404();
+    exit;
+}
+
+/*
+*	Блок проверки авторизации
+*/
+$oUser = Core::factory("User")->getCurent();
+
+if($oUser != true)
+{
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = $_SERVER["REQUEST_URI"];
+    header("Location: http://$host$uri/authorize?back=$host$uri"."$extra");
+    exit;
+}
+
+
 $aTitle[] = $this->oStructure->title();
 
 if(get_class($this->oStructureItem) == "User_Group")
