7ab6acdcb7fe42a5fdf1ed97eaa2177fb3893945
egor <creative27016@gmail.com>
2018-04-16 02:01:10 +0300

    Summary

--- controller/s_index.php
+++ controller/s_index.php
@@ -1,10 +1,1 @@
 <?php
-
-$cookieData = "1";
-
-$cookieTime = 3600 * 24;
-if(REMEMBER_USER_TIME != "")    $cookieTime *= REMEMBER_USER_TIME;
-
-//setcookie("user_data", $cookieData, time() + $cookieTime);
-
-//setcookie("user_data", "Hello world", time() - 3600);
