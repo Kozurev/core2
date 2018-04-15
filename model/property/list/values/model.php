50ea4b7467f5c1fe5528589c855d4a4645e5bf49
egor <creative27016@gmail.com>
2018-04-16 02:04:02 +0300

    Исправлены сеттеры и геттеры

--- model/property/list/values/model.php
+++ model/property/list/values/model.php
@@ -14,60 +14,29 @@ class Property_List_Values_Model extends Core_Entity
 
 	public function property_id($val = null)
 	{
-		try
-		{
-			if(is_null($val))
-			{
-				return $this->property_id;
-			}
-			
-			if(is_integer($val))
-			{
-				$this->property_id = $val;
-				return $this;
-			}
-			
-			throw new Exception(INVALID_TYPE);
-		}
-		catch(Exception $e)
-		{
-			echo "<br>".$e->getMessage();
-		}
+		if(is_null($val))   return $this->property_id;
+
+		$this->property_id = intval($val);
+		return $this;
 	}
 
 
 	public function value($val = null)
 	{
-		try
-		{
-			if(is_null($val))
-			{
-				return $this->value;
-			}
-			
-			if(!is_array($val) && !is_object($val))
-			{
-				if(strlen($val) <= 100)
-				{
-					$this->value = $val;
-					return $this;
-				}
-				else
-					throw new Exception(TOO_LARGE_VALUE);
-			}
-			throw new Exception(INVALID_TYPE);
-		}
-		catch(Exception $e)
-		{
-			echo "<br>".$e->getMessage();
-		}
+		if(is_null($val))   return $this->value;
+
+		if(strlen($val) > 100)
+		    die(Core::getMessage("TOO_LARGE_VALUE", array("value", "Property_List_Values", 100)));
+
+		$this->value = $val;
+		return $this;
 	}
 
 
     public function sorting($val = null)
     {
         if(is_null($val))	return $this->sorting;
-        if(!is_int($val))   die(Core::getMessage("INVALID_TYPE", array("sorting", "Property_Values", "int")));
+        //if(!is_int($val))   die(Core::getMessage("INVALID_TYPE", array("sorting", "Property_Values", "int")));
         $this->sorting = intval($val);
         return $this;
     }
