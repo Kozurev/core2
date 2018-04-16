4b632d9de3121553ef24b2f5eae5f828522ccee0
Kozurev <creative27016@gmail.com>
2018-04-16 17:41:05 +0300

    Добавлена библиотека для сортировки таблицы

--- controller/c_index.php
+++ controller/c_index.php
@@ -1,9 +1,8 @@
 <?php
 
+//Выгрузка (обновление) пользователей
+$pdoString = "";
+$user = "";
+$pass = "";
+//$conn = new PDO($pdoString, $user, $pass);
 
-$oProperty = Core::factory("Property");
-$oUserGroup = Core::factory("Structure", 5);
-$aoGroupProperties = $oProperty->getPropertiesList($oUserGroup);
-
-echo "<pre>";
-print_r($aoGroupProperties);
