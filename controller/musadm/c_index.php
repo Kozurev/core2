958fe2d59abed92c0eae5f07b3fb7e305e0f16a4
Kozurev <creative27016@gmail.com>
2018-04-16 17:42:04 +0300

    Добавлено разделение ролей

--- controller/musadm/c_index.php
+++ controller/musadm/c_index.php
@@ -6,3 +6,31 @@
  * Time: 21:21
  */
 
+$oCurentUser = Core::factory("User")->getCurent();
+$pageUserId = Core_Array::getValue($_GET, "userid", 0); //id просматриваемого пользователя администратором
+
+$bOnlyAdmin = false;
+$bAdminWithUser = false;
+
+//Если администратор авторизован под учетной записью пользователя
+if($oCurentUser->groupId() < 4 && $pageUserId > 0)
+{
+    $oUser = Core::factory("User", $pageUserId);
+}
+else
+{
+    $oUser = $oCurentUser;
+}
+
+if($oCurentUser->groupId() < 4 && $pageUserId)
+{
+    $bAdminWithUser = true;
+    echo "<h1>Администратор под записью пользователя</h1>";
+}
+else
+{
+    echo "<h1>Только администратор</h1>";
+}
+
+$oUserGroup = Core::factory("User_Group", $oUser->groupId());
+
