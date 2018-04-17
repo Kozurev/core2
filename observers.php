efb36722d7774528764ecb261d346f6e68c70f01
Kozurev <creative27016@gmail.com>
2018-04-17 17:45:37 +0300

    Добавление наблюдателей, связанных с добавлением значений в список

--- observers.php
+++ observers.php
@@ -6,15 +6,28 @@
  * Time: 13:52
  */
 
-// Core::attachObserver("afterUserSave", function($args){
-//     $oNewUser = $args[0];
-//     $oProperty = Core::factory("Property");
-//     $oUserGroup = Core::factory("User_Group", $oNewUser->groupId());
-//     $aoGroupProperties = $oProperty->getPropertiesList($oUserGroup);
 
-//     foreach ($aoGroupProperties as $prop)
-//     {
-//         $oProperty->addToPropertiesList($oNewUser, $prop->getId());
-//     }
+Core::attachObserver("afterUserSave", function($args){
 
-// });
+    $oUser = $args[0];
+    if($oUser->groupId() != 4 && $oUser->getId() == "") return;
+
+    $teacherFullName = $oUser->surname() . " " . $oUser->name();
+    $listValue = Core::factory("Property_List_Values")
+        ->property_id(21)
+        ->value($teacherFullName)
+        ->save();
+
+});
+
+Core::attachObserver("beforeUserDelete", function($args){
+
+    $oUser = $args[0];
+    $listValue = Core::factory("Property_List_Values")
+        ->where("property_id", "=", 21)
+        ->where("value", "like", "%".$oUser->name()."%")
+        ->where("value", "like", "%".$oUser->surname()."%")
+        ->find();
+
+    if($listValue) $listValue->delete();
+});
