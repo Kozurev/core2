04af4dbccdd143dbcbac895f9391cd92bcd09881
egor <creative27016@gmail.com>
2018-04-20 01:38:24 +0300

    Добавлены методы для работы с макетами + установлены наблюдатели

--- model/page/template.php
+++ model/page/template.php
@@ -5,4 +5,34 @@
 class Page_Template extends Page_Template_Model
 {
 
+    public function getParent()
+    {
+        if($this->parent_id == "")  return false;
+        return Core::factory("Page_Template", $this->parent_id);
+    }
+
+
+    public function getChildren()
+    {
+        if($this->id == "") return false;
+        return Core::factory("Page_Template")
+            ->where("parent_id", "=", $this->id)
+            ->findAll();
+    }
+
+
+    public function delete($obj = null)
+    {
+        Core::notify(array(&$this), "beforeTemplateDelete");
+        parent::delete();
+        Core::notify(array(&$this), "afterTemplateDelete");
+    }
+
+
+    public function save($obj = null)
+    {
+        Core::notify(array(&$this), "beforeTemplateSave");
+        parent::save();
+        Core::notify(array(&$this), "afterTemplateSave");
+    }
 }
