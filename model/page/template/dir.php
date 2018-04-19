a29375a9782d249b78a944b27cb0a38a53eaea95
egor <creative27016@gmail.com>
2018-04-20 01:38:46 +0300

    Добавлены методы для работы с директориями макетов + установлены наблюдатели

---
+++ model/page/template/dir.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kozurev Egor
+ * Date: 19.04.2018
+ * Time: 16:30
+ */
+
+class Page_Template_Dir extends Page_Template_Dir_Model
+{
+
+    public function getParent()
+    {
+        if($this->dir == "")  return false;
+        return Core::factory("Page_Template_Dir", $this->dir);
+    }
+
+
+    public function getChildren()
+    {
+        if($this->id == "") return false;
+
+        $aoDirs = Core::factory("Page_Template_Dir")
+            ->where("dir", "=", $this->id)
+            ->findAll();
+
+        $aoTemplates = Core::factory("Page_Template")
+            ->where("dir", "=", $this->id)
+            ->findAll();
+
+        return array_merge($aoDirs, $aoTemplates);
+    }
+
+
+    public function delete($obj = null)
+    {
+        Core::notify(array(&$this), "beforeTemplateDirDelete");
+        parent::delete();
+        Core::notify(array(&$this), "afterTemplateDirDelete");
+    }
+
+
+    public function save($obj = null)
+    {
+        Core::notify(array(&$this), "beforeTemplateDirSave");
+        parent::save();
+        Core::notify(array(&$this), "afterTemplateDirSave");
+    }
+}
