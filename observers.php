a5b48d4eec789d78c6bfeb97ce152298dbe84a47
egor <creative27016@gmail.com>
2018-04-20 01:41:02 +0300

    Добавлены наблюдатели
    1)  Рекурсивное удаление макетов
    2) Рекурсивное удаление директорий макетов
    3) Добавление элементов в список доп. свойства при регистрации учителя
    4) Удаление элемента из списка доп. свойства при удалении учителя
    5) Запись последней авторизации для клиентов

--- observers.php
+++ observers.php
@@ -6,22 +6,25 @@
  * Time: 13:52
  */
 
-
-Core::attachObserver("afterUserSave", function($args){
-
+/**
+ * Добавление ФИО преподавателя в список дополнительного свойства "Преподаватель"
+ */
+Core::attachObserver("beforeUserSave", function($args){
     $oUser = $args[0];
     if($oUser->groupId() != 4 && $oUser->getId() == "") return;
-
     $teacherFullName = $oUser->surname() . " " . $oUser->name();
+
     $listValue = Core::factory("Property_List_Values")
         ->property_id(21)
         ->value($teacherFullName)
         ->save();
-
 });
 
-Core::attachObserver("beforeUserDelete", function($args){
 
+/**
+ * Удаление пункта списка дополнительного свойства "Преподаватель"
+ */
+Core::attachObserver("beforeUserDelete", function($args){
     $oUser = $args[0];
     $listValue = Core::factory("Property_List_Values")
         ->where("property_id", "=", 21)
@@ -30,4 +33,46 @@ Core::attachObserver("beforeUserDelete", function($args){
         ->find();
 
     if($listValue) $listValue->delete();
+});
+
+
+/**
+ * Рекурсивное удаление вложенных макетов и диекторий при удалении директории
+ */
+Core::attachObserver("beforeTemplateDirDelete", function($args){
+    $aoChildren = $args[0]->getChildren();
+    foreach ($aoChildren as $child) $child->delete();
+});
+
+
+/**
+ * Рекурсивное удаление вложенных макетов при удалении макета
+ */
+Core::attachObserver("beforeTemplateDelete", function($args){
+    $aoChildren = $args[0]->getChildren();
+    foreach ($aoChildren as $child) $child->delete();
+});
+
+
+/**
+ * Запись даты/времени последней авторизации пользователя
+ */
+Core::attachObserver("afterUserAuthorize", function($args){
+    $oUser = $args[0];
+    if($oUser != false && $oUser->groupId() == 5)
+    {
+        $oProperty = Core::factory("Property", 22);
+        $oProperty->addToPropertiesList($oUser, 22);
+        $now = date("d-m-Y H:i:s");
+
+        $value = $oProperty->getPropertyValues($oUser)[0];
+        if($value->getId())
+        {
+            $value->value($now)->save();
+        }
+        else
+        {
+            $oProperty->addNewValue($oUser, $now);
+        }
+    }
 });
