5f3336f50921eaaf877de4bfd7b91ff864c30941
Kozurev <creative27016@gmail.com>
2018-04-20 16:44:52 +0300

    Платежи

---
+++ model/payment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 20.04.2018
+ * Time: 15:05
+ */
+
+class Payment extends Payment_Model
+{
+
+}
---
+++ model/payment/model.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 20.04.2018
+ * Time: 15:06
+ */
+
+class Payment_Model extends Core_Entity
+{
+    protected $id;
+    protected $user;
+    protected $type;
+    protected $datetime;
+    protected $value;
+    protected $description;
+
+    public function __construct()
+    {
+        $this->datetime = date("d-m-Y H:i:s");
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function user($val = null)
+    {
+        if(is_null($val))   return $this->user;
+
+        $this->user = intval($val);
+        return $this;
+    }
+
+
+    public function type($val = null)
+    {
+        if(is_null($val))   return $this->type;
+        if($val == true)    $this->type = 1;
+        elseif($val == false)$this->type = 0;
+        return $this;
+    }
+
+
+    public function datetime($val = null)
+    {
+        if(is_null($val))   return $this->datetime;
+        $this->datetime = $val;
+        return $this;
+    }
+
+
+    public function value($val = null)
+    {
+        if(is_null($val))   return $this->value;
+        $this->value = intval($val);
+        return $this;
+    }
+
+
+    public function description($val = null)
+    {
+        if(is_null($val))   return $this->description;
+        $this->description= $val;
+        return $this;
+    }
+
+}
