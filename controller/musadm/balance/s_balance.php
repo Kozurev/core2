b4ca8cff0a2f408b2b131a23d6734737a22a97c5
egor <creative27016@gmail.com>
2018-04-23 11:39:27 +0300

    обработчики для ajax-запросов

--- controller/musadm/balance/s_balance.php
+++ controller/musadm/balance/s_balance.php
@@ -24,3 +24,43 @@ if(isset($_GET["ajax"]) && $_GET["ajax"] == 1)
     $this->execute();
     exit;
 }
+
+$action = Core_Array::getValue($_GET, "action", "");
+
+if($action == "add_note")
+{
+    $modelId = Core_Array::getValue($_GET,"model_id", 0);
+    $oPayment = Core::factory("Payment", $modelId);
+    $aoNotes = Core::factory("Property", 26)->getPropertyValues($oPayment);
+
+    Core::factory("Core_Entity")
+        ->addEntity($oPayment)
+        ->addEntities($aoNotes, "notes")
+        ->xsl("musadm/balance/add_payment_note.xsl")
+        ->show();
+
+    exit;
+}
+
+if($action == "refreshTablePayments")
+{
+    $userId = Core_Array::getValue($_GET, "user_id", 0);
+    $aoUserPayments = Core::factory("Payment")
+        ->orderBy("id", "DESC")
+        ->where("user", "=", $userId)
+        //->where("value", ">", "0")
+        ->findAll();
+
+    foreach ($aoUserPayments as $payment)
+    {
+        $aoUserPaymentsNotes = Core::factory("Property", 26)->getPropertyValues($payment);
+        $payment->addEntities($aoUserPaymentsNotes, "notes");
+    }
+
+    Core::factory("Core_Entity")
+        ->addEntities($aoUserPayments)
+        ->xsl("musadm/balance/payments.xsl")
+        ->show();
+
+    exit;
+}
