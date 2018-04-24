6463669291d63e96b672589ec7efcbc1c69770eb
egor <creative27016@gmail.com>
2018-04-25 01:23:01 +0300

    Выгрузка лидов и всего что с ними связанно

--- controller/c_test.php
+++ controller/c_test.php
@@ -6,12 +6,36 @@
  * Time: 1:02
  */
 
-$oProperty = Core::factory("Property", 12);
-$oUser = Core::factory("User", 301);
+$dbh = new mysqli("37.140.192.32:3306", "u4834_ADMIN", "big#psKT", "u4834955_musbase");
+$dbh->query("SET NAMES utf8");
 
-$oParentDir = Core::factory("User_Group", 5);
-$parentProperties = Core::factory("Property")->getPropertiesList($oParentDir);
-
-echo "<pre>";
-//print_r($oProperty->getPropertiesList($oUser));
-print_r($oProperty->getPropertyValues($oUser));
+Core::factory("Orm")->executeQuery("TRUNCATE `Lid`");
+//Core::factory("Orm")->executeQuery("TRUNCATE `Lid_Comment`");
+//
+//$aoLids = Core::factory("Lid")->findAll();
+//$oProperty = Core::factory("Property");
+//foreach ($aoLids as $lid) $oProperty->clearForObject($lid);
+//
+//
+//
+//$aoLids = $dbh->query("SELECT * FROM lids");
+//$oProperty = Core::factory("Property", 27);
+//
+//while($lid = $aoLids->fetch_object())
+//{
+//    $oLid = Core::factory("Lid")
+//        ->name($lid->name)
+//        ->surname($lid->surname)
+//        ->number($lid->phone)
+//        ->vk($lid->vk);
+//
+//    $oLid->save();
+//
+//    $oProperty->addNewValue($oLid, intval($lid->status) + 79);
+//
+//    Core::factory("Lid_Comment")
+//        ->authorId(1)
+//        ->lidId($oLid->getId())
+//        ->text($lid->note)
+//        ->save();
+//}
