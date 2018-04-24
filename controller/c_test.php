3a084567d252ec3934ad2c9222a00a4d9521add7
Kozurev <creative27016@gmail.com>
2018-04-24 16:14:09 +0300

    Summary

--- controller/c_test.php
+++ controller/c_test.php
@@ -6,3 +6,12 @@
  * Time: 1:02
  */
 
+$oProperty = Core::factory("Property", 12);
+$oUser = Core::factory("User", 301);
+
+$oParentDir = Core::factory("User_Group", 5);
+$parentProperties = Core::factory("Property")->getPropertiesList($oParentDir);
+
+echo "<pre>";
+//print_r($oProperty->getPropertiesList($oUser));
+print_r($oProperty->getPropertyValues($oUser));
