d8a1b438c6c8b0d63288eee9ae2de88151ef2da7
egor <creative27016@gmail.com>
2018-04-25 01:27:41 +0300

    Исправлен баг, не позволявший осуществлять поиск

--- templates/template3/js/js.js
+++ templates/template3/js/js.js
@@ -3,14 +3,14 @@ $(function(){
         .on("click", ".user_search_submit", function(){
             var search_data = $(".user_search_input").val();
             var group_id = $("#group_id").val();
-            var link = "admin?menuTab=Main&menuAction=show&group_id=" + group_id + "&search=" + search_data + "&parent_id=" + group_id + "&parent_name=User_Group";
-            if($("#createData").valid())
+            var link = "admin?menuTab=User&menuAction=show&group_id=" + group_id + "&search=" + search_data + "&parent_id=" + group_id + "&parent_name=User_Group";
+            //if($("#createData").valid())
                 window.location.hash = "#" + link;
         })
         .on("click", ".payment_search_submit", function(){
             var search_data = $(".payment_search_input").val();
             var link = "admin?menuTab=Payment&menuAction=show&search=" + search_data;
-            if($("#createData").valid())
+            //if($("#createData").valid())
                 window.location.hash = "#" + link;
         });
 });
