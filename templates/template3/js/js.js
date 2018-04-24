7613c85f863028fa8beb15f9233251c137ba5586
Kozurev <creative27016@gmail.com>
2018-04-24 16:17:01 +0300

    Добавлен обработчик для поиска по платежам

--- templates/template3/js/js.js
+++ templates/template3/js/js.js
@@ -1,9 +1,16 @@
 $(function(){
-    $(document).on("click", ".user_search_submit", function(){
-        var search_data = $(".user_search_input").val();
-        var group_id = $("#group_id").val();
-        var link = "admin?menuTab=User&menuAction=show&group_id=" + group_id + "&search=" + search_data + "&parent_id=" + group_id + "&parent_name=User_Group";
-        if(search_data == "")   return;
-        window.location.hash = "#" + link;
-    });
+    $(document)
+        .on("click", ".user_search_submit", function(){
+            var search_data = $(".user_search_input").val();
+            var group_id = $("#group_id").val();
+            var link = "admin?menuTab=Main&menuAction=show&group_id=" + group_id + "&search=" + search_data + "&parent_id=" + group_id + "&parent_name=User_Group";
+            if($("#createData").valid())
+                window.location.hash = "#" + link;
+        })
+        .on("click", ".payment_search_submit", function(){
+            var search_data = $(".payment_search_input").val();
+            var link = "admin?menuTab=Payment&menuAction=show&search=" + search_data;
+            if($("#createData").valid())
+                window.location.hash = "#" + link;
+        });
 });
