8566c736e18e38b3976bb0d66bc634ea7abbee32
egor <creative27016@gmail.com>
2018-04-25 01:25:47 +0300

    Модель группы учеников

---
+++ model/schedule/group.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Egor
+ * Date: 24.04.2018
+ * Time: 19:59
+ */
+
+class Schedule_Group extends Schedule_Group_Model
+{
+
+}
---
+++ model/schedule/group/model.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Egor
+ * Date: 24.04.2018
+ * Time: 20:00
+ */
+
+class Schedule_Group_Model extends Core_Entity
+{
+    protected $id;
+    protected $teacher_id;
+    protected $title;
+
+
+    public function __construct(){}
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function teacherId($val = null)
+    {
+        if(is_null($val))   return $this->teacher_id;
+        $this->teacher_id = intval($val);
+        return $this;
+    }
+
+
+    public function title($val = null)
+    {
+        if(is_null($val))   return $this->title;
+        if(strlen($val) > 255)
+            die(Core::getMessage("TOO_LARGE_VALUE", array("title", "Schedule_Group", 255)));
+        $this->title = strval($val);
+        return $this;
+    }
+
+}
