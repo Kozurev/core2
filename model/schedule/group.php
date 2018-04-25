9640a878e79c49254b180ef74197f2b946125f8e
egor <creative27016@gmail.com>
2018-04-26 02:33:16 +0300

    Обработчики и наблюдатели для работы с группами клиентов

--- model/schedule/group.php
+++ model/schedule/group.php
@@ -8,5 +8,73 @@
 
 class Schedule_Group extends Schedule_Group_Model
 {
+    public function getClientList()
+    {
+        if($this->id == null)   return array();
+
+        $assignments = Core::factory("Schedule_Group_Assignment")
+            ->where("group_id", "=", $this->id)
+            ->findAll();
+
+        $output = array();
+
+        foreach ($assignments as $assignment)
+        {
+            $aoGroupUsers = Core::factory("User")
+                ->where("id", "=", $assignment->userId())
+                ->findAll();
+
+            $output = array_merge($output, $aoGroupUsers);
+        }
+
+        return $output;
+    }
+
+
+    public function clearClientList()
+    {
+        if($this->id == null)   return;
+
+        $assignments = Core::factory("Schedule_Group_Assignment")
+            ->where("group_id", "=", $this->id)
+            ->findAll();
+
+        foreach ($assignments as $assignment)   $assignment->delete();
+    }
+
+
+    public function getTeacher()
+    {
+        if($this->id == null)   return false;
+        return Core::factory("User", $this->teacher_id);
+    }
+
+
+    public function appendClient($userid)
+    {
+        if($this->id == null)   return;
+
+        Core::factory("Schedule_Group_Assignment")
+            ->groupId($this->id)
+            ->userId($userid)
+            ->save();
+    }
+
+
+    public function delete($obj = null)
+    {
+        Core::notify(array(&$this), "beforeScheduleGroupDelete");
+        $this->clearClientList();
+        parent::delete();
+        Core::notify(array(&$this), "ScheduleGroupDelete");
+    }
+
+
+    public function save($obj = null)
+    {
+        Core::notify(array(&$this), "beforeScheduleGroupSave");
+        parent::save();
+        Core::notify(array(&$this), "afterScheduleGroupSave");
+    }
 
 }
