78b39a43c1da2b5e0db9c27122048bf02dce8f3f
egor <creative27016@gmail.com>
2018-04-26 02:33:55 +0300

    Обработчики для работы с группами пользователей

---
+++ templates/template4/js/groups.js
@@ -0,0 +1,55 @@
+$(function(){
+    $(document)
+        .on("click", ".group_delete", function(e){
+            e.preventDefault();
+            loaderOn();
+            var groupid = $(this).data("groupid");
+            deleteItem("Schedule_Group", groupid, refreshGroupTable);
+        })
+        .on("click", ".group_edit", function(e){
+            e.preventDefault();
+            var groupid = $(this).data("groupid");
+            getGroupPopup(groupid);
+        })
+        .on("click", ".popop_group_submit", function(e){
+            e.preventDefault();
+            loaderOn();
+            saveData("groups", refreshGroupTable);
+        })
+        .on("click", ".group_create", function(e){
+            e.preventDefault();
+            getGroupPopup(0);
+        });
+});
+
+
+
+function refreshGroupTable(){
+    $.ajax({
+        type: "GET",
+        url: "groups",
+        data: {
+            action: "refreshGroupTable",
+        },
+        success: function(responce) {
+            $(".page").empty();
+            $(".page").append(responce);
+            $("#sortingTable").tablesorter();
+            loaderOff();
+        }
+    });
+}
+
+function getGroupPopup(groupid) {
+    $.ajax({
+        type: "GET",
+        url: "groups",
+        data: {
+            action: "updateForm",
+            groupid: groupid
+        },
+        success: function(responce) {
+            showPopup(responce);
+        }
+    });
+}
