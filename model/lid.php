099d282e0b0de639df39a1903e7fc45a7e35081a
Kozurev <creative27016@gmail.com>
2018-04-26 15:55:35 +0300

    Добавлены новые методы

--- model/lid.php
+++ model/lid.php
@@ -14,4 +14,46 @@ class Lid extends Lid_Model
         parent::save();
         Core::notify(array(&$this), "afterLidSave");
     }
+
+
+    public function delete($obj = null)
+    {
+        Core::notify(array(&$this), "beforeLidDelete");
+
+        if($this->id != null)
+        {
+            $aoComments = Core::factory("Lid_Comment")
+                ->where("lid_id", "=", $this->id)
+                ->findAll();
+
+            foreach ($aoComments as $comment)   $comment->delete();
+
+            Core::factory("Property")->clearForObject($this);
+        }
+
+        parent::delete();
+        Core::notify(array(&$this), "afterLidDelete");
+    }
+
+
+    public function getComments()
+    {
+        if($this->id == null)   return array();
+
+        $aoComments = Core::factory("Lid_Comment")
+            ->where("lid_id", "=", $this->id)
+            ->findAll();
+
+        return $aoComments;
+    }
+
+
+    public function getStatusList()
+    {
+        return Core::factory("Property_List_Values")
+            ->where("property_id", "=", 27)
+            ->findAll();
+    }
+
+
 }
