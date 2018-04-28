6e1b61ed75c6ff5d37f13e7cd858b84a6d253c55
Kozurev <creative27016@gmail.com>
2018-04-28 17:08:20 +0300

    Добавлены обработчики для работы с платежаами

--- templates/template4/js/payments.js
+++ templates/template4/js/payments.js
@@ -18,8 +18,30 @@ $(function(){
             e.preventDefault();
             loaderOn();
             var userid = $(this).data("userid");
-            saveData("admin");
+            saveData("admin?menuTab=Main&menuAction=updateAction&ajax=1", loaderOff);
             refreshPaymentsTable(userid, loaderOff);
+        })
+        .on("click", ".btn_balance", function(e){
+            e.preventDefault();
+            var userid = $(this).data("userid");
+            getPaymentPopup(userid, "balance");
+        })
+        .on("click", ".popop_balance_payment_submit", function(e){
+            e.preventDefault();
+            loaderOn();
+            var form = $("#createData");
+            if($(form).valid() == false)
+            {
+                loaderOff();
+                return;
+            }
+            var userid = $(this).data("userid");
+            var value = $(form).find("input[name=value]").val();
+            var description = $(form).find("textarea[name=description]").val();
+            var type = $(form).find("input[name=type]:checked").val();
+            savePayment(userid, value, description, type, "balance", loaderOn);
+            refreshPaymentsTable(userid, loaderOff);
+            //refreshBalanceTable(userid, loaderOff);
         });
 });
 
@@ -30,10 +52,15 @@ function refreshPaymentsTable(userid, func) {
         url: "balance",
         data: {
             action: "refreshTablePayments",
-            user_id: userid
+            user_id: userid,
+            userid: userid
         },
         success: function(responce) {
-            $("#sortingTable").remove();
+            // $("#sortingTable").remove();
+            // $(".page").append(responce);
+            // $("#sortingTable").tablesorter();
+            // func();
+            $(".page").empty();
             $(".page").append(responce);
             $("#sortingTable").tablesorter();
             func();
