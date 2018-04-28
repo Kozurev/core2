264c5a2541e6bd4e3fb699a3931866dd4b25d465
Kozurev <creative27016@gmail.com>
2018-04-28 17:03:27 +0300

    Sunnary

--- controller/c_test.php
+++ controller/c_test.php
@@ -9,33 +9,16 @@
 $dbh = new mysqli("37.140.192.32:3306", "u4834_ADMIN", "big#psKT", "u4834955_musbase");
 $dbh->query("SET NAMES utf8");
 
-Core::factory("Orm")->executeQuery("TRUNCATE `Lid`");
-//Core::factory("Orm")->executeQuery("TRUNCATE `Lid_Comment`");
+//Core::factory("Orm")->executeQuery("TRUNCATE Payment_Tarif");
+//$aoTarifs = $dbh->query("SELECT * FROM `ref_packet`");
 //
-//$aoLids = Core::factory("Lid")->findAll();
-//$oProperty = Core::factory("Property");
-//foreach ($aoLids as $lid) $oProperty->clearForObject($lid);
-//
-//
-//
-//$aoLids = $dbh->query("SELECT * FROM lids");
-//$oProperty = Core::factory("Property", 27);
-//
-//while($lid = $aoLids->fetch_object())
+//while ($tarif = $aoTarifs->fetch_object())
 //{
-//    $oLid = Core::factory("Lid")
-//        ->name($lid->name)
-//        ->surname($lid->surname)
-//        ->number($lid->phone)
-//        ->vk($lid->vk);
-//
-//    $oLid->save();
-//
-//    $oProperty->addNewValue($oLid, intval($lid->status) + 79);
-//
-//    Core::factory("Lid_Comment")
-//        ->authorId(1)
-//        ->lidId($oLid->getId())
-//        ->text($lid->note)
+//    Core::factory("Payment_Tarif")
+//        ->title($tarif->name)
+//        ->price($tarif->price)
+//        ->lessonsCount($tarif->numberlesson)
+//        ->lessonsType($tarif->typelessonid)
+//        ->access(!intval($tarif->onlyadmin))
 //        ->save();
 //}
