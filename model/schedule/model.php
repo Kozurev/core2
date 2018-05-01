6f0b0ba7eff7c0ff3892c07c4be32941ab1eaaae
egor <creative27016@gmail.com>
2018-05-02 01:19:53 +0300

    Модель контроллера расписания

--- model/schedule/model.php
+++ model/schedule/model.php
@@ -8,5 +8,17 @@
 
 class Schedule_Model extends Core_Entity
 {
+    //protected $type;    //main or common
+    protected $date;    //date format Y-m-d (0000-00-00)
+    protected $area;    //area id
+    protected $teacher; //user id
+    protected $client;  //user id
+
+
+    public function date($val = null)
+    {
+
+    }
+
 
 }
