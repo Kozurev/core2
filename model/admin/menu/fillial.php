8fc1f2659df029e63c1e709c28e41c1c97331b4e
egor <creative27016@gmail.com>
2018-05-02 01:17:36 +0300

    Работа с филиалами в админ. меню

---
+++ model/admin/menu/fillial.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kozurev Egor
+ * Date: 01.05.2018
+ * Time: 11:49
+ */
+
+class Admin_Menu_Fillial
+{
+    public function show($aParams)
+    {
+        $aoAreas = Core::factory("Schedule_Area")->findAll();
+        Core::factory("Core_Entity")
+            ->addEntities($aoAreas)
+            ->xsl("admin/schedule/areas.xsl")
+            ->show();
+    }
+}
