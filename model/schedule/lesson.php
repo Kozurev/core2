2c1e51c6d5b602c1846cf23201fddb2b6d6daf01
egor <creative27016@gmail.com>
2018-05-02 01:19:24 +0300

    Обработчики для работы с расписанием

--- model/schedule/lesson.php
+++ model/schedule/lesson.php
@@ -6,7 +6,22 @@
  * Time: 19:57
  */
 
-class Schedule_Lesson extends Scedule_Lesson_Model
+class Schedule_Lesson extends Schedule_Lesson_Model
 {
+    public function getGroup()
+    {
+        if($this->group_id != "")   return Core::factory("Schedule_Group", $this->group_id);
+    }
 
+
+    public function getTeacher()
+    {
+        if($this->teacher_id != "") return Core::factory("User", $this->teacher_id);
+    }
+
+
+    public function getClient()
+    {
+        if($this->client_id != "")  return Core::factory("User", $this->client_id);
+    }
 }
