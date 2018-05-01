306ecca932cd9aa4b017cb814d8c91e3e40867b5
egor <creative27016@gmail.com>
2018-05-02 01:16:01 +0300

    Вынесена переадресация с кода страницы в настройки

--- controller/musadm/s_index.php
+++ controller/musadm/s_index.php
@@ -24,3 +24,41 @@ if(isset($_GET["ajax"]) && $_GET["ajax"] == 1)
     $this->execute();
     exit;
 }
+
+/**
+ * Настроки редиректа
+ */
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+$oCurentUser = Core::factory("User")->getCurent();
+$pageUserId = Core_Array::getValue($_GET, "userid", 0); //id просматриваемого пользователя администратором
+
+//Если администратор авторизован под учетной записью пользователя
+if($oCurentUser->groupId() < 4 && $pageUserId > 0)
+{
+    $oUser = Core::factory("User", $pageUserId);
+}
+else
+{
+    $oUser = $oCurentUser;
+}
+
+if($oCurentUser->groupId() < 4 && $pageUserId)
+{
+    header("Location: http://$host$uri/schedule?area=2&userid=".$pageUserId);
+    exit;
+    //echo "<h1>Администратор под записью пользователя</h1>";
+}
+elseif($oCurentUser->groupId() < 4)
+{
+	header("Location: http://$host$uri/user/client");
+    exit;
+    //echo "<h1>Только администратор</h1>";
+}
+elseif($oCurentUser->groupId() > 3 && !$pageUserId)
+{
+	header("Location: http://$host$uri/schedule");
+	exit;
+    //echo "<h1>Только пользователь</h1>";
+}
