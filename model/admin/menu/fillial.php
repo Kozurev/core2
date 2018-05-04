77bfe025da0b1ab8d1506c4958e46d02e728fdfe
Kozurev <creative27016@gmail.com>
2018-05-04 15:49:04 +0300

    Добавлена сортировка

--- model/admin/menu/fillial.php
+++ model/admin/menu/fillial.php
@@ -10,7 +10,7 @@ class Admin_Menu_Fillial
 {
     public function show($aParams)
     {
-        $aoAreas = Core::factory("Schedule_Area")->findAll();
+        $aoAreas = Core::factory("Schedule_Area")->orderBy("sorting")->findAll();
         Core::factory("Core_Entity")
             ->addEntities($aoAreas)
             ->xsl("admin/schedule/areas.xsl")
