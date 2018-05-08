7714c5a30679d89c7dec7c88050ae1e2bb2f7280
Kozurev <creative27016@gmail.com>
2018-05-08 16:50:23 +0300

    Файл для пользовательских подключений различных библиотек

---
+++ includes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kozurev Egor
+ * Date: 08.05.2018
+ * Time: 13:27
+ */
+
+require_once ROOT."/lib/functions.php";
+require_once ROOT."/lib/schedule_functions.php";
