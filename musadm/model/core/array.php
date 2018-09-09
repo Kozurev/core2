df5a90bcbe9a786ab39588e3f2d0c1f61940197a
egor <creative27016@gmail.com>
2018-09-09 20:03:36 +0300

    Добавлены методы для работы с массивами $_GET и $_POST
    ost

--- musadm/model/core/array.php
+++ musadm/model/core/array.php
@@ -1,20 +1,55 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: User
- * Date: 20.03.2018
- * Time: 16:03
+ * Класс для работы с массивами
+ *
+ * @author Bad Wolf
+ * @date 20.03.2018 16:03
  */
 
 class Core_Array
 {
-    public static function getValue($arr, $key, $default)
+    /**
+     * Получение значения элемента из массива по ключу
+     *
+     * @param $arr - исходный массив
+     * @param $key - ключь
+     * @param $default - значение по умолчанию
+     * @return mixed
+     */
+    public static function getValue( $arr, $key, $default )
     {
-        if(isset($arr[$key]) && $arr[$key] != "")
+        if( isset( $arr[$key] ) && $arr[$key] != "" )
         {
             return $arr[$key];
         }
         else return $default;
     }
+
+
+    /**
+     * Метод для получения значения из массива $_GET
+     *
+     * @param $key - ключь
+     * @param $default - значение по умолчанию
+     * @return mixed
+     */
+    public static function Get( $key, $default )
+    {
+        return self::getValue( $_GET, $key, $default );
+    }
+
+
+    /**
+     * Метод для получения значения из массива $_POST
+     *
+     * @param $key - ключь
+     * @param $default - значение по умолчанию
+     * @return mixed
+     */
+    public static function Post( $key, $default )
+    {
+        return self::getValue( $_POST, $key, $default );
+    }
+
 
 }
