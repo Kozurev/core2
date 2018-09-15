0dd5b6daa3733b83ddd3945c150b9d9f11df4b8c
Kozurev <creative27016@gmail.com>
2018-09-15 16:21:45 +0300

    Изменены условия показа панели с заметками и датой последней авторизации

--- musadm/templates/template6/template.php
+++ musadm/templates/template6/template.php
@@ -6,17 +6,19 @@ $this->css( "templates/template6/css/style.css" );
  * Вывод панели с заметками пользователя и датой последней авторизации 
  */
 $oCurentUser = Core::factory("User")->getCurrent();
-$pageUserId = Core_Array::getValue($_GET, "userid", 0);
+//$pageUserId = Core_Array::getValue($_GET, "userid", 0);
+User::isAuthAs() ? $isAdmin = 1 : $isAdmin = 0;
 
-if($oCurentUser->groupId() < 4 && $pageUserId > 0)
-    $oUser = Core::factory("User", $pageUserId);
-else
+//if($oCurentUser->groupId() < 4 && $pageUserId > 0)
+//    $oUser = Core::factory("User", $pageUserId);
+//else
     $oUser = $oCurentUser;
 
 /**
  * Пользовательские примечания и дата последней авторизации
  */
-if($oCurentUser->groupId() < 4 && $oUser->groupId() == 5)
+//if($oCurentUser->groupId() < 4 && $oUser->groupId() == 5)
+if( $isAdmin && $oCurentUser->groupId() == 5 )
 {
     $oPropertyNotes = Core::factory("Property", 19);
     $clienNotes = $oPropertyNotes->getPropertyValues($oUser);
