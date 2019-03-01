90b367d2dfa4a73cd2b64ab2de4dd2ae1ba0dd44
Kozurev <creative27016@gmail.com>
2019-03-01 19:32:29 +0300

    Рефакторинг старого кода

--- musadm/model/property/list/model.php
+++ musadm/model/property/list/model.php
@@ -17,7 +17,10 @@ class Property_List_Model extends Core_Entity
 
 	public function property_id( $val = null )
 	{
-		if ( is_null( $val ) )	return $this->property_id;
+		if ( is_null( $val ) )
+        {
+            return intval( $this->property_id );
+        }
 
 		$this->property_id = intval( $val );
 
@@ -27,7 +30,10 @@ class Property_List_Model extends Core_Entity
 
 	public function model_name( $val = null )
 	{
-		if ( is_null( $val ) ) 	return $this->model_name;
+		if ( is_null( $val ) )
+        {
+            return $this->model_name;
+        }
 
 		if ( strlen( $val ) > 100 )
         {
@@ -42,7 +48,10 @@ class Property_List_Model extends Core_Entity
 
 	public function object_id( $val = null )
 	{
-		if ( is_null( $val ) )	return intval( $this->object_id );
+		if ( is_null( $val ) )
+        {
+            return intval( $this->object_id );
+        }
 
 		$this->object_id = intval( $val );
 
@@ -55,6 +64,50 @@ class Property_List_Model extends Core_Entity
         if ( is_null( $val ) )	return intval( $this->value_id );
 
         $this->value_id = intval( $val );
+
+        return $this;
+    }
+
+
+    public function propertyId( $val = null )
+    {
+        if ( is_null( $val ) )
+        {
+            return intval( $this->property_id );
+        }
+
+        $this->property_id = intval( $val );
+
+        return $this;
+    }
+
+
+    public function modelName( $val = null )
+    {
+        if ( is_null( $val ) )
+        {
+            return $this->model_name;
+        }
+
+        if ( strlen( $val ) > 100 )
+        {
+            exit ( Core::getMessage( 'TOO_LARGE_VALUE', ['model_name', 'Property_List', 100] ) );
+        }
+
+        $this->model_name = $val;
+
+        return $this;
+    }
+
+
+    public function objectId( $val = null )
+    {
+        if ( is_null( $val ) )
+        {
+            return intval( $this->object_id );
+        }
+
+        $this->object_id = intval( $val );
 
         return $this;
     }
--- musadm/model/property/model.php
+++ musadm/model/property/model.php
@@ -5,39 +5,43 @@
 class Property_Model extends Core_Entity
 {
 	protected $id;
-	protected $tag_name; //
-	protected $title; //
-	protected $description = " "; //
-	protected $type = 0; //
+	protected $tag_name;
+	protected $title;
+	protected $description = ' ';
+	protected $type = 0;
     protected $multiple = 0;
-	protected $active = 1; //
+	protected $active = 1;
     protected $sorting = 0;
     protected $dir = 0;
     protected $default_value = 0;
 
-	public function __construct()
-	{
-		
-	}
 
 
-	public function getId(){
-		return $this->id;}
+	public function getId()
+    {
+		return intval( $this->id );
+    }
 
 
     public function defaultValue($val = null)
     {
-        if(is_null($val))   return $this->default_value;
+        if ( is_null( $val ) )
+        {
+            return $this->default_value;
+        }
 
         $this->default_value = $val;
         return $this;
     }
 
 
-
-	public function active($val = null)
+	public function active( $val = null )
 	{
-		if(is_null($val))   return $this->active;
+		if ( is_null( $val ) )
+        {
+            return $this->active;
+        }
+
 		$val == true
             ?   $this->active = 1
             :   $this->active = 0;
@@ -46,76 +50,143 @@ class Property_Model extends Core_Entity
 	}
 
 
-	public function multiple($val = null)
+	public function multiple( $val = null )
     {
-        if(is_null($val))   return $this->multiple;
+        if ( is_null( $val ) )
+        {
+            return $this->multiple;
+        }
 
-        if($val == true)    $this->multiple = 1;
-        else                $this->multiple = 0;
+        if ( $val == true )
+        {
+            $this->multiple = 1;
+        }
+        else
+        {
+            $this->multiple = 0;
+        }
 
         return $this;
     }
 
 
-	public function dir($val = null)
+	public function dir( $val = null )
     {
-        if(is_null($val))   return $this->dir;
-        if(intval($val) < 0)   die(Core::getMessage("UNSIGNED_VALUE", array("dir", "Property")));
-        $this->dir = intval($val);
+        if ( is_null( $val ) )
+        {
+            return $this->dir;
+        }
+
+        if ( intval( $val ) < 0 )
+        {
+            die ( Core::getMessage( 'UNSIGNED_VALUE', ['dir', 'Property'] ) );
+        }
+
+        $this->dir = intval( $val );
         return $this;
     }
 
 
-	public function title($val = null)
+	public function title( $val = null )
 	{
-		if(is_null($val))   return $this->title;
-		if(strlen($val) > 150)
-		    die(Core::getMessage("TOO_LARGE_VALUE", array("title", "Property", 150)));
+		if ( is_null( $val ) )
+        {
+            return $this->title;
+        }
+
+		if ( strlen( $val ) > 150 )
+        {
+            die ( Core::getMessage( 'TOO_LARGE_VALUE', ['title', 'Property', 150] ) );
+        }
+
 		$this->title = $val;
 		return $this;
 	}
 
 
-	public function tag_name($val = null)
+	public function tag_name( $val = null )
 	{
-		if(is_null($val))   return $this->tag_name;
-		if(strlen($val) > 50)
-		    die(Core::getMessage("TOO_LARGE_VALUE", array("tag_name", "Property", 50)));
+		if ( is_null( $val ) )
+        {
+            return $this->tag_name;
+        }
+
+		if ( strlen( $val ) > 50 )
+        {
+            die ( Core::getMessage( 'TOO_LARGE_VALUE', ['tag_name', 'Property', 50] ) );
+        }
+
 		$this->tag_name = $val;
 		return $this;
 	}
 
 
-	public function description($val = null)
+    public function tagName( $val = null )
+    {
+        if ( is_null( $val ) )
+        {
+            return $this->tag_name;
+        }
+
+        if ( strlen( $val ) > 50 )
+        {
+            die ( Core::getMessage( 'TOO_LARGE_VALUE', ['tag_name', 'Property', 50] ) );
+        }
+
+        $this->tag_name = $val;
+        return $this;
+    }
+
+
+	public function description( $val = null )
 	{
-		if(is_null($val))   return $this->description;
+		if ( is_null( $val ) )
+        {
+            return $this->description;
+        }
+
 		$this->description = $val;
 		return $this;
 	}
 
 
-	public function type($val = null)
+	public function type( $val = null )
 	{
-		if(is_null($val))   return $this->type;
-		if(strlen($val) > 50)
-		    die(Core::getMessage("TOO_LARGE_VALUE", array("type", "Property", 50)));
+		if ( is_null( $val ) )
+        {
+            return $this->type;
+        }
+
+		if ( strlen( $val ) > 50 )
+        {
+            die ( Core::getMessage( 'TOO_LARGE_VALUE', ['type', 'Property', 50] ) );
+        }
+
 		$this->type = $val;
 		return $this;
 	}
 
 
-    public function sorting($val = null)
+    public function sorting( $val = null )
     {
-        if(is_null($val))   return $this->sorting;
-        if(intval($val) < 0)   die(Core::getMessage("UNSIGNED_VALUE", array("sorting", "Property")));
-        $this->sorting = intval($val);
+        if ( is_null( $val ) )
+        {
+            return $this->sorting;
+        }
+
+        if ( intval( $val ) < 0 )
+        {
+            die ( Core::getMessage( 'UNSIGNED_VALUE', ['sorting', 'Property'] ) );
+        }
+
+        $this->sorting = intval( $val );
         return $this;
     }
 
 
     public function getPropertyTypes()
     {
-        return array("Int", "String", "Text", "List", "Bool");
+        return ['Int', 'String', 'Text', 'List', 'Bool'];
     }
 
 }
