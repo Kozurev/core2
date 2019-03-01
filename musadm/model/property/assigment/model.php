6f36194916955220729218ad0961e776757e9b42
Kozurev <creative27016@gmail.com>
2019-03-01 19:31:31 +0300

    Рефакторинг старого кода

--- musadm/model/property/assigment/model.php
+++ musadm/model/property/assigment/model.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Egor
- * Date: 07.04.2018
- * Time: 2:22
+ *
+ *
+ * @author Bad Wolf
+ * @date 07.04.2018 2:22
+ * @version 20190225
+ * Class Property_Assigment_Model
  */
-
 class Property_Assigment_Model extends Core_Entity
 {
     protected $id;
@@ -15,35 +16,42 @@ class Property_Assigment_Model extends Core_Entity
 
     public function getId()
     {
-        return $this->id;
+        return intval( $this->id );
     }
 
 
-    public function object_id($val = null)
+    public function object_id( $val = null )
     {
-        if(is_null($val))   return $this->object_id;
-        //if(!is_int($val))   die(Core::getMessage("INVALID_TYPE", array("object_id", "Property_Assigment", "integer")));
-        if(intval($val) <= 0)       die(Core::getMessage("UNSIGNED_VALUE", array("object_id", "Property_Assigment")));
-        $this->object_id = intval($val);
+        if ( is_null( $val ) )
+        {
+            return intval( $this->object_id );
+        }
+
+        $this->object_id = intval( $val );
         return $this;
     }
 
 
-    public function model_name($val = null)
+    public function model_name( $val = null )
     {
-        if(is_null($val))   return $this->model_name;
-        if(strlen(strval($val)) > 255)  die(Core::getMessage("TOO_LARGE_VALUE", array("model_name", "Property_Assigment", "255")));
+        if ( is_null( $val ) )
+        {
+            return $this->model_name;
+        }
+
         $this->model_name = $val;
         return $this;
     }
 
 
-    public function property_id($val = null)
+    public function property_id( $val = null )
     {
-        if(is_null($val))   return $this->property_id;
-        //if(!is_int($val))   die(Core::getMessage("INVALID_TYPE", array("object_id", "Property_Assigment", "integer")));
-        if(intval($val) <= 0)       die(Core::getMessage("UNSIGNED_VALUE", array("property_id", "Property_Assigment")));
-        $this->property_id = intval($val);
+        if ( is_null( $val ) )
+        {
+            return $this->property_id;
+        }
+
+        $this->property_id = intval( $val );
         return $this;
     }
 
