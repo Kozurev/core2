4dcec76fbc4918898163b6af6009ff25b258ee51
Kozurev <creative27016@gmail.com>
2019-03-22 18:50:24 +0300

    метод .on в schedule.js удален и добавлен аттрибут onclick

--- musadm/templates/template7/template.php
+++ musadm/templates/template7/template.php
@@ -1,68 +1,69 @@
 <?php
+/**
+ * Макет для раздела "Расписание"
+ *
+ * @author BadWolf
+ * @version 20190322
+ */
 
-    $pageUserId = Core_Array::Get( 'userid', null, PARAM_INT );
-    $subordinated = User::current()->getDirector()->getId();
+$pageUserId = Core_Array::Get('userid', null, PARAM_INT);
+$subordinated = User::current()->getDirector()->getId();
 
-    if ( is_null( $pageUserId ) )
+if (is_null($pageUserId)) {
+    $User = User::current();
+} else {
+    Core::factory('User_Controller');
+    $User = User_Controller::factory($pageUserId);
+}
+
+is_object(Core_Page_Show::instance()->StructureItem)
+    ?   $areaId = Core_Page_Show::instance()->StructureItem->getId()
+    :   $areaId = 0;
+
+    if (User::checkUserAccess(['groups' => [ROLE_MANAGER]], $User)
+        || (User::checkUserAccess(['groups' => [ROLE_DIRECTOR]], $User) && is_object(Core_Page_Show::instance()->StructureItem))
+    )
     {
-        $User = User::current();
-    }
-    else
-    {
-        Core::factory( 'User_Controller' );
-        $User = User_Controller::factory( $pageUserId );
+        Core_Page_Show::instance()->css('/templates/template7/css/style.css');
     }
 
-    is_object( Core_Page_Show::instance()->StructureItem )
-        ?   $areaId = Core_Page_Show::instance()->StructureItem->getId()
-        :   $areaId = 0;
-
-        if (
-            User::checkUserAccess( ['groups' => [ROLE_MANAGER]], $User )
-            || ( User::checkUserAccess( ['groups' => [ROLE_DIRECTOR]], $User ) && is_object( Core_Page_Show::instance()->StructureItem ) )
-        )
-        {
-            Core_Page_Show::instance()->css( '/templates/template7/css/style.css' );
-        }
-
-        if (
-            User::checkUserAccess( ['groups' => [ROLE_MANAGER, ROLE_TEACHER]], $User )
-            || ( User::checkUserAccess( ['groups' => [ROLE_DIRECTOR]], $User ) && is_object( Core_Page_Show::instance()->StructureItem ) )
-        ) { ?>
-        <section>
-            <div class="row calendar_small">
-                <div>
-                    <input type="date" class="form-control schedule_calendar" />
-                </div>
-                <div>
-                    <span class="day_name"></span>
-                </div>
-                <div>
-                    <a class="btn btn-green schedule_task_create">Написать администратору</a>
-                </div>
+    if (User::checkUserAccess(['groups' => [ROLE_MANAGER, ROLE_TEACHER]], $User)
+        || (User::checkUserAccess(['groups' => [ROLE_DIRECTOR]], $User) && is_object(Core_Page_Show::instance()->StructureItem))
+    ) { ?>
+    <section>
+        <div class="row calendar_small">
+            <div>
+                <input type="date" class="form-control schedule_calendar" />
             </div>
-        </section>
-    <? }else { ?>
-        <script>
-            $(function(){
-                var date = new Date();
-                var year = date.getFullYear();
-                var month = date.getMonth() + 1;
-
-                if( month < 10 ) month = "0" + month;
-
-                $("#year").val(year);
-                $("#month").val(month);
-            });
-        </script>
-    <? } ?>
-
-    <section class="schedule">
-        <?php
-        Core_Page_Show::instance()->execute();
-        ?>
+            <div>
+                <span class="day_name"></span>
+            </div>
+            <div>
+                <a class="btn btn-green" onclick="newScheduleTaskPopup()">Написать администратору</a>
+            </div>
+        </div>
     </section>
+<? }else { ?>
+    <script>
+        $(function(){
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
 
-    <input type="hidden" id="userid" value="<?=$User->getId()?>" />
-    <input type="hidden" id="areaid" value="<?=$areaId?>" />
+            if( month < 10 ) month = '0' + month;
+
+            $('#year').val(year);
+            $('#month').val(month);
+        });
+    </script>
+<? } ?>
+
+<section class="schedule">
+    <?php
+    Core_Page_Show::instance()->execute();
+    ?>
+</section>
+
+<input type="hidden" id="userid" value="<?=$User->getId()?>" />
+<input type="hidden" id="areaid" value="<?=$areaId?>" />
 
