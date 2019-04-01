0f2012fc813dcd04cb8596e61b0040f74a906bb9
Kozurev <creative27016@gmail.com>
2019-04-01 17:26:32 +0300

    Рефакторинг старого кода

--- musadm/controller/musadm/tasks/c_index.php
+++ musadm/controller/musadm/tasks/c_index.php
@@ -1,27 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Kozurev Egor
- * Date: 16.05.2018
- * Time: 17:07
+ * Файл формирующий контент раздела лидов
+ *
+ * @author BadWolf
+ * @date 16.05.2018 17:07
+ * @version 20190401
  */
 
-$from = Core_Array::Get( 'date_from', null );
-$to =   Core_Array::Get( 'date_to', null );
-$today = date( 'Y-m-d' );
-
+$today = date('Y-m-d');
+$from = Core_Array::Get('date_from', null, PARAM_STRING);
+$to =   Core_Array::Get('date_to', null, PARAM_STRING);
 
 $Director = User::current()->getDirector();
 $subordinated = $Director->getId();
 
-
-Core::factory( 'Task_Controller' );
-$TaskController = new Task_Controller( User::current() );
+Core::factory('Task_Controller');
+$TaskController = new Task_Controller(User::current());
 $TaskController
-    ->periodFrom( $from )
-    ->periodTo( $to )
-    ->isShowPeriods( true )
-    ->isSubordinate( true )
-    ->isLimitedAreasAccess( true )
-    ->addSimpleEntity( 'taskAfterAction', 'tasks' )
+    ->periodFrom($from)
+    ->periodTo($to)
+    ->isShowPeriods(true)
+    ->isSubordinate(true)
+    ->isLimitedAreasAccess(true)
+    ->addSimpleEntity('taskAfterAction', 'tasks')
     ->show();
