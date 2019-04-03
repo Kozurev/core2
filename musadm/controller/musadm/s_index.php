f5b8b747f2fcef3d94d1543a9e490c011e6be576
Kozurev <creative27016@gmail.com>
2019-04-03 16:28:00 +0300

    Рефакторинг старого кода

--- musadm/controller/musadm/s_index.php
+++ musadm/controller/musadm/s_index.php
@@ -11,82 +11,65 @@
 $User = User::current();
 $access = ['groups' => [ROLE_ADMIN, ROLE_MANAGER, ROLE_DIRECTOR]];
 
-if ( $User === null )
-{
-    $host  = Core_Array::Server( 'HTTP_HOST', '' );
-    $uri   = rtrim( dirname( Core_Array::Server( 'PHP_SELF', '' ) ), '/\\' );
-    header( "Location: http://$host$uri/authorize?back=$host$uri/" );
+if (is_null($User)) {
+    $host  = Core_Array::Server('HTTP_HOST', '');
+    $uri   = rtrim(dirname(Core_Array::Server('PHP_SELF', '', PARAM_STRING)), '/\\');
+    header("Location: http://$host$uri/authorize?back=$host$uri/");
     exit;
 }
 
-$host  = Core_Array::Server( 'HTTP_HOST', '' );
-$uri   = rtrim( dirname( Core_Array::Server( 'PHP_SELF', '' ) ), '/\\' );
+$host  = Core_Array::Server('HTTP_HOST', '', PARAM_STRING);
+$uri   = rtrim(dirname(Core_Array::Server('PHP_SELF', '', PARAM_STRING)), '/\\');
 
-Core_Page_Show::instance()->setParam( 'body-class', 'body-green' );
-Core_Page_Show::instance()->setParam( 'title-first', 'ГЛАВНАЯ' );
-Core_Page_Show::instance()->setParam( 'title-second', 'СТРАНИЦА' );
+Core_Page_Show::instance()->setParam('body-class', 'body-green');
+Core_Page_Show::instance()->setParam('title-first', 'ГЛАВНАЯ');
+Core_Page_Show::instance()->setParam('title-second', 'СТРАНИЦА');
 
-if ( Core_Array::Get( 'ajax', null, PARAM_STRING ) === null )
-{
-    if ( !User::checkUserAccess( $access, $User ) )
-    {
-        header( "Location: http://$host$uri/authorize?back=/$uri" );
+if (Core_Array::Get('ajax', null, PARAM_STRING) === null) {
+    if (!User::checkUserAccess($access, $User)) {
+        header("Location: http://$host$uri/authorize?back=/$uri");
     }
-
-    if ( $User->groupId() == 6 )
-    {
-        header( "Location: http://$host$uri/user/client" );
+    if ($User->groupId() == ROLE_DIRECTOR) {
+        header("Location: http://$host$uri/user/client");
     }
-
-    if ( $User->groupId() == 5 )
-    {
-        header( "Location: http://$host$uri/balance" );
+    if ($User->groupId() == ROLE_CLIENT) {
+        header("Location: http://$host$uri/balance");
     }
-
-    if ( $User->groupId() == 4 )
-    {
-        header( "Location: http://$host$uri/schedule" );
+    if ($User->groupId() == ROLE_TEACHER) {
+        header("Location: http://$host$uri/schedule");
     }
 }
 
 
-$action = Core_Array::Get(  'action', null, PARAM_STRING );
-
+$action = Core_Array::Get('action', null, PARAM_STRING);
 
 $Director = $User->getDirector();
 $subordinated = $Director->getId();
 
-Core::factory( 'User_Controller' );
-Core::factory( 'Lid_Controller' );
-Core::factory( 'Task_Controller' );
-Core::factory( 'Property_Controller' );
+Core::factory('User_Controller');
+Core::factory('Lid_Controller');
+Core::factory('Task_Controller');
+Core::factory('Property_Controller');
 
 
 /**
  * Обработчик для открытия всплывающего окна редактирования списка дополнительного свойства
  */
-if ( $action === 'getPropertyListPopup' )
-{
-    $propId = Core_Array::Get( 'prop_id', null, PARAM_INT );
-
-    if ( $propId === null )
-    {
-        Core_Page_Show::instance()->error( 404 );
+if ($action === 'getPropertyListPopup') {
+    $propId = Core_Array::Get('prop_id', null, PARAM_INT);
+    if (is_null($propId)) {
+        Core_Page_Show::instance()->error(404);
     }
 
-    $Property = Property_Controller::factory( $propId );
-
-    if ( $Property === null )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    $Property = Property_Controller::factory($propId);
+    if (is_null($Property)) {
+        Core_Page_Show::instance()->error(404);
     }
 
-    $Values = $Property->getList();
-
-    Core::factory( 'Core_Entity' )
-        ->addEntity( $Property )
-        ->addEntities( $Values )
-        ->xsl( 'musadm/edit_property_list.xsl' )
+    Core::factory('Core_Entity')
+        ->addEntity($Property)
+        ->addEntities($Property->getList())
+        ->xsl('musadm/edit_property_list.xsl')
         ->show();
 
     exit;
@@ -96,36 +79,31 @@ if ( $action === 'getPropertyListPopup' )
 /**
  * Сохранение элемента списка дополнительного свойства
  */
-if ( $action === 'savePropertyListValue' )
-{
-    $id =     Core_Array::Get( 'id', null, PARAM_INT );
-    $propId = Core_Array::Get( 'prop_id', null, PARAM_INT );
-    $value =  Core_Array::Get( 'value', null, PARAM_STRING );
+if ($action === 'savePropertyListValue') {
+    $id =     Core_Array::Get('id', null, PARAM_INT);
+    $propId = Core_Array::Get('prop_id', null, PARAM_INT);
+    $value =  Core_Array::Get('value', null, PARAM_STRING);
 
-    if ( $propId === null || $value === null || $propId <= 0 || $value == '' )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    if (is_null($propId) || is_null($value) || $propId <= 0 || $value == '') {
+        Core_Page_Show::instance()->error(404);
     }
 
-    $NewValue = Property_Controller::factoryListValue( $id );
-
-    if ( $NewValue === null )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    $NewValue = Property_Controller::factoryListValue($id);
+    if (is_null($NewValue)) {
+        Core_Page_Show::instance()->error(404);
     }
 
     $NewValue
-        ->propertyId( $propId )
-        ->value( $value )
-        ->sorting( 0 );
+        ->propertyId($propId)
+        ->value($value)
+        ->sorting(0);
     $NewValue->save();
 
     $returnJson = new stdClass();
     $returnJson->id = $NewValue->getId();
     $returnJson->propertyId = $propId;
     $returnJson->value = $value;
-
-    echo json_encode( $returnJson );
+    echo json_encode($returnJson);
     exit;
 }
 
@@ -133,24 +111,18 @@ if ( $action === 'savePropertyListValue' )
 /**
  * Удаление элемента списка дополнения свйоства
  */
-if ( $action === 'deletePropertyListValue' )
-{
-    $id = Core_Array::Get( 'id', null, PARAM_INT );
-
-    if ( $id === null )
-    {
-        Core_Page_Show::instance()->error( 404 );
+if ($action === 'deletePropertyListValue') {
+    $id = Core_Array::Get('id', null, PARAM_INT);
+    if (is_null($id)) {
+        Core_Page_Show::instance()->error(404);
     }
 
-    $PropertyListValue = Property_Controller::factoryListValue( $id );
-
-    if ( $PropertyListValue === null )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    $PropertyListValue = Property_Controller::factoryListValue($id);
+    if (is_null($PropertyListValue)) {
+        Core_Page_Show::instance()->error(404);
     }
 
     $PropertyListValue->delete();
-
     exit;
 }
 
@@ -158,39 +130,37 @@ if ( $action === 'deletePropertyListValue' )
 /**
  * Обработчик для сохранения значения доп. свойства
  */
-if ( $action === 'savePropertyValue' )
-{
-    $propertyName = Core_Array::Get( 'prop_name', null, PARAM_STRING );
-    $propertyValue= Core_Array::Get( 'value', null, PARAM_FLOAT );
-    $modelId =      Core_Array::Get( 'model_id', null, PARAM_INT );
-    $modelName =    Core_Array::Get( 'model_name', null, PARAM_STRING );
+if ($action === 'savePropertyValue') {
+    $propertyName = Core_Array::Get('prop_name', null, PARAM_STRING);
+    $propertyValue= Core_Array::Get('value', null, PARAM_FLOAT);
+    $modelId =      Core_Array::Get('model_id', null, PARAM_INT);
+    $modelName =    Core_Array::Get('model_name', null, PARAM_STRING);
 
-    $Object = Core::factory( $modelName );
-    $Property = Core::factory( 'Property' )->getByTagName( $propertyName );
+    $Object = Core::factory($modelName);
+    $Property = Core::factory('Property')->getByTagName($propertyName);
 
-    if ( is_null( $Property ) || is_null( $Object ) )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    if (is_null($Property) || is_null($Object)) {
+        Core_Page_Show::instance()->error(404);
     }
 
-    if ( method_exists( $Object, 'subordinated' ) )
-    {
+    if (method_exists($Object, 'subordinated')) {
         $Object->queryBuilder()
-            ->where( 'subordinated', '=', $subordinated );
+            ->open()
+            ->where('subordinated', '=', $subordinated)
+            ->orWhere('subordinated', '=', 0)
+            ->close();
     }
 
     $Object = $Object->queryBuilder()
-        ->where( 'id', '=', $modelId )
+        ->where('id', '=', $modelId)
         ->find();
 
-    if ( is_null( $Object ) )
-    {
-        Core_Page_Show::instance()->error( 404 );
+    if (is_null($Object)) {
+        Core_Page_Show::instance()->error(404);
     }
 
-    $Value = $Property->getPropertyValues( $Object )[0];
-    $Value->value( $propertyValue )->save();
-
+    $Value = $Property->getPropertyValues($Object)[0];
+    $Value->value($propertyValue)->save();
     exit;
 }
 
@@ -198,16 +168,12 @@ if ( $action === 'savePropertyValue' )
 /**
  * Обновление таблицы лидов
  */
-if ( $action === 'refreshLidTable' )
-{
-    $LidController = new Lid_Controller( $User );
+if ($action === 'refreshLidTable') {
+    $LidController = new Lid_Controller($User);
     $LidController
-        ->lidId(
-            Core_Array::Get( 'lidid', null, PARAM_INT )
-        )
-        ->isShowPeriods( false )
+        ->lidId(Core_Array::Get('lidid', null, PARAM_INT))
+        ->isShowPeriods(false)
         ->show();
-
     exit;
 }
 
@@ -215,83 +181,70 @@ if ( $action === 'refreshLidTable' )
 /**
  * Обновление таблицы
  */
-if( $action === 'refreshTasksTable' )
-{
-    $TaskController = new Task_Controller( User::current() );
+if ($action === 'refreshTasksTable') {
+    $TaskController = new Task_Controller(User::current());
     $TaskController
-        ->isShowPeriods( false )
-        ->isSubordinate( true )
-        ->isLimitedAreasAccess( true )
+        ->isShowPeriods(false)
+        ->isSubordinate(true)
+        ->isLimitedAreasAccess(true)
         ->show();
-
     exit;
 }
 
 
-if ( $action === 'search_client' )
-{
-    $surname = Core_Array::Get( 'surname', null, PARAM_STRING );
-    $name    = Core_Array::Get( 'name', null, PARAM_STRING );
-    $phone   = Core_Array::Get( 'phone', null, PARAM_STRING );
+if ($action === 'search_client') {
+    $surname = Core_Array::Get('surname', null, PARAM_STRING);
+    $name    = Core_Array::Get('name', null, PARAM_STRING);
+    $phone   = Core_Array::Get('phone', null, PARAM_STRING);
 
-    $ClientController = new User_Controller( User::current() );
+    $ClientController = new User_Controller(User::current());
     $ClientController
-        ->isSubordinate( true )
-        ->filterType( User_Controller::FILTER_NOT_STRICT )
-        ->isActiveBtnPanel( false )
-        ->groupId( 5 )
-        ->properties( true )
-        ->isLimitedAreasAccess( true )
-        ->xsl( 'musadm/users/clients.xsl' );
+        ->isSubordinate(true)
+        ->filterType(User_Controller::FILTER_NOT_STRICT)
+        ->isActiveBtnPanel(false)
+        ->groupId(ROLE_CLIENT)
+        ->properties(true)
+        ->isLimitedAreasAccess(true)
+        ->xsl('musadm/users/clients.xsl');
 
-    if ( !is_null( $surname ) )
-    {
-        $ClientController->appendFilter( 'surname', $surname );
+    if (!is_null($surname)) {
+        $ClientController->appendFilter('surname', $surname);
+    }
+    if (!is_null($name)) {
+        $ClientController->appendFilter('name', $name);
+    }
+    if (!is_null($phone)) {
+        $ClientController->appendFilter('phone_number', $phone);
     }
 
-    if ( !is_null( $name ) )
-    {
-        $ClientController->appendFilter( 'name', $name );
-    }
-
-    if ( !is_null( $phone ) )
-    {
-        $ClientController->appendFilter( 'phone_number', $phone );
-    }
-
-    $SearchingClientsHtml = $ClientController->show( false );
-
-    if ( $ClientController->count() > 0 )
-    {
+    $SearchingClientsHtml = $ClientController->show(false);
+    if ($ClientController->count() > 0) {
         echo "<div class='users'>";
         echo $SearchingClientsHtml;
         echo "</div>";
     }
-
     exit;
 }
 
 
-if ( $action === 'getObjectInfoPopup' )
-{
-    $id =     Core_Array::Get( 'id', 0, PARAM_INT );
-    $model =  Core_Array::Get( 'model', '', PARAM_STRING );
+if ($action === 'getObjectInfoPopup') {
+    $id =     Core_Array::Get('id', 0, PARAM_INT);
+    $model =  Core_Array::Get('model', '', PARAM_STRING);
 
-    $Object = Core::factory( $model )
+    $Object = Core::factory($model)
         ->queryBuilder()
-        ->where( 'id', '=', $id )
-        ->where( 'subordinated', '=', $subordinated )
+        ->where('id', '=', $id)
+        ->where('subordinated', '=', $subordinated)
         ->find();
 
-    if ( $Object === null )
-    {
-        exit ( "<h2>Объект с переданными данными не найден или был удален</h2>" );
+    if (is_null($Object)) {
+        exit("<h2>Объект с переданными данными не найден или был удален</h2>");
     }
 
-    $Output = Core::factory( 'Core_Entity' )
-        ->xsl( 'musadm/object.xsl' );
+    $Output = Core::factory('Core_Entity')
+        ->xsl('musadm/object.xsl');
 
-    switch ( $model )
+    switch ($model)
     {
         case 'Task' :
             $TaskController = new Task_Controller( User::current() );
@@ -305,49 +258,44 @@ if ( $action === 'getObjectInfoPopup' )
             exit;
 
         case 'Lid' :
-            $LidController = new Lid_Controller( User::current() );
+            $LidController = new Lid_Controller(User::current());
             $LidController
-                ->isShowPeriods( false )
-                ->isShowButtons( false )
-                ->isPeriodControl( false )
-                ->lidId( $id )
-                ->xsl( 'musadm/lids/lids.xsl' )
+                ->isShowPeriods(false)
+                ->isShowButtons(false )
+                ->isPeriodControl(false)
+                ->lidId($id)
+                ->xsl('musadm/lids/lids.xsl')
                 ->show();
             exit;
 
         case 'Certificate' :
-            $Object->sellDate( refactorDateFormat( $Object->sellDate() ) );
-            $Object->activeTo( refactorDateFormat( $Object->activeTo() ) );
+            $Object->sellDate(refactorDateFormat($Object->sellDate()));
+            $Object->activeTo(refactorDateFormat($Object->activeTo()));
 
             $Notes = Core::factory( 'Certificate_Note' )->queryBuilder()
-                ->select( [ 'certificate_id', 'author_id', 'date', 'text', 'surname', 'name'] )
-                ->where( 'certificate_id', '=', $id )
-                ->leftJoin( 'User AS u', 'u.id = author_id' )
-                ->orderBy( 'date', 'DESC' )
-                ->orderBy( 'Certificate_Note.id', 'DESC' )
+                ->select(['certificate_id', 'author_id', 'date', 'text', 'surname', 'name'])
+                ->where('certificate_id', '=', $id)
+                ->leftJoin('User AS u', 'u.id = author_id')
+                ->orderBy('date', 'DESC')
+                ->orderBy('Certificate_Note.id', 'DESC')
                 ->findAll();
 
-            foreach ( $Notes as $Note )
-            {
-                $Note->date( refactorDateFormat( $Note->date() ) );
+            foreach ($Notes as $Note) {
+                $Note->date(refactorDateFormat($Note->date()));
             }
 
-            $Output->addEntities( $Notes, 'note' );
+            $Output->addEntities($Notes, 'note');
             break;
 
         default: echo "<h2>Ошибка: отсутствует обработчик для модели '". $model ."'</h2>";
     }
 
-    $Output
-        ->addEntity( $Object )
-        ->show();
-
+    $Output->addEntity($Object)->show();
     exit;
 }
 
 
-if ( $action === 'refreshTableUsers' )
-{
+if ($action === 'refreshTableUsers') {
     Core_Page_Show::instance()->execute();
     exit;
 }
