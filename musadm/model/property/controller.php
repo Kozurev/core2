6b16461b499ee546a7658c95918ee0c3f6d9bf28
egor <creative27016@gmail.com>
2019-07-12 16:49:18 +0300

    Рефакторинг старого кода

--- musadm/model/property/controller.php
+++ musadm/model/property/controller.php
@@ -5,6 +5,7 @@
  * @author Bad Wolf
  * @date 22.02.2019 9:54
  * @version 20190222
+ * @version 20190712
  * Class Property_Controller
  */
 class Property_Controller
@@ -32,20 +33,19 @@ class Property_Controller
      * @param string|null $type
      * @return Property|Property_Int|Property_String|Property_Bool|Property_Text|Property_List|null
      */
-    public static function factory( int $id = null, string $type = null )
+    public static function factory(int $id = null, string $type = null)
     {
-        if ( !is_null( $type ) && !in_array( $type, self::$types ) )
-        {
+        if (!is_null($type) && !in_array($type, self::$types)) {
             return null;
         }
 
-        is_null( $type )
+        is_null($type)
             ?   $className = 'Property'
             :   $className = 'Property_' . $type;
 
-        is_null( $id ) || $id === 0
-            ?   $obj = Core::factory( $className )
-            :   $obj = Core::factory( $className, intval( $id ) );
+        is_null($id) || $id === 0
+            ?   $obj = Core::factory($className)
+            :   $obj = Core::factory($className, intval($id));
 
         return $obj;
     }
@@ -59,37 +59,28 @@ class Property_Controller
      * @param bool $isSubordinate
      * @return Property_List_Values|null
      */
-    public static function factoryListValue( int $id = null, bool $isSubordinate = true )
+    public static function factoryListValue(int $id = null, bool $isSubordinate = true)
     {
-        if ( is_null( $id ) || $id === 0 )
-        {
-            return Core::factory( 'Property_List_Values' );
+        if (is_null($id) || $id === 0) {
+            return Core::factory('Property_List_Values');
         }
 
-        $ResValue = Core::factory( 'Property_List_Values' )
+        $ResValue = Core::factory('Property_List_Values')
             ->queryBuilder()
-            ->where( 'id', '=', $id );
+            ->where('id', '=', $id);
 
-        if ( $isSubordinate === true )
-        {
+        if ($isSubordinate === true) {
             $AuthUser = User::current();
-
-            if ( is_null( $AuthUser ) )
-            {
+            if (is_null($AuthUser)) {
                 return null;
             }
-
             $Director = $AuthUser->getDirector();
-
-            if ( is_null( $Director ) )
-            {
+            if (is_null($Director)) {
                 return null;
             }
-
-            $ResValue->where( 'subordinated', '=', $Director->getId() );
+            $ResValue->where('subordinated', '=', $Director->getId());
         }
 
         return $ResValue->find();
     }
-
 }
