6c738bd24c7d88c65c5dee2d5a9cd1a7a1be2a6a
egor <creative27016@gmail.com>
2019-07-15 22:04:38 +0300

    Добавлен новый класс-контроллер для работы с лидами наследующий родительский класс Controller

--- musadm/model/lid/controller/extended.php
+++ musadm/model/lid/controller/extended.php
@@ -52,7 +52,7 @@ class Lid_Controller_Extended extends Controller
      *
      * @var bool
      */
-    private $isShowPeriods = true;
+    protected $isShowPeriods = true;
 
 
     /**
@@ -60,8 +60,13 @@ class Lid_Controller_Extended extends Controller
      *
      * @var bool
      */
-    private $isShowButtons = true;
+    protected $isShowButtons = true;
 
+
+    /**
+     * @var bool
+     */
+    protected $isEnableCommonLids = true;
 
 
 
@@ -133,8 +138,8 @@ class Lid_Controller_Extended extends Controller
 
 
     /**
-     * @param string $to
-     * @return $this
+     * @param string|null $to
+     * @return $this|string
      */
     public function periodTo(string $to = null)
     {
@@ -170,6 +175,21 @@ class Lid_Controller_Extended extends Controller
     }
 
 
+    /**
+     * @param bool|null $isEnable
+     * @return $this|bool
+     */
+    public function isEnableCommonLids(bool $isEnable = null)
+    {
+        if (is_null($isEnable)) {
+            return $this->isEnableCommonLids;
+        } else {
+            $this->isEnableCommonLids = $isEnable;
+            return $this;
+        }
+    }
+
+
 
     public function getLids()
     {
@@ -178,11 +198,39 @@ class Lid_Controller_Extended extends Controller
         }
 
         if ($this->isEnabledPeriodControl()) {
-            if (!empty($this->periodFrom())) {
-                $this->appendFilter('control_date', '>=', $this->periodFrom());
+            if ($this->periodFrom() == $this->periodTo()) {
+                $this->appendFilter('control_date', '=', $this->periodFrom());
+            } else {
+                if (!empty($this->periodFrom())) {
+                    $this->appendFilter('control_date', '>=', $this->periodFrom());
+                }
+                if (!empty($this->periodTo())) {
+                    $this->appendFilter('control_date', '<=', $this->periodTo());
+                }
             }
-            if (!empty($this->periodTo())) {
-                $this->appendFilter('control_date', '>=', $this->periodTo());
+        }
+
+        //Формирование условий выборки лидов по филиалам
+        if (count($this->areasIds) > 0) {
+            //$ForAreas = $this->areasIds;
+        } elseif ($this->isLimitedAreasAccess === true && !is_null($this->User) && $this->User->groupId() !== ROLE_DIRECTOR) {
+            $Areas = Core::factory('Schedule_Area_Assignment')
+                ->getAreas($this->User, true);
+            foreach ($Areas as $Area) {
+                $this->areasIds[] = $Area->getId();
+            }
+        } else {
+            $this->areasIds = [];
+        }
+
+        if (count($this->areasIds) > 0) {
+            if ($this->isEnableCommonLids == true) {
+                $this->QueryBuilder->open()
+                    ->where('area_id', '=', 0)
+                    ->orWhereIn('area_id', $this->areasIds)
+                    ->close();
+            } else {
+                $this->QueryBuilder->whereIn('area_id', $this->areasIds);
             }
         }
 
@@ -192,31 +240,11 @@ class Lid_Controller_Extended extends Controller
             $this->foundObjectsIds[] = $Lid->getId();
         }
 
+        //Фильтрация по значениям доп.свйотв
+        $this->addFilterExecute($Lids);
+
         //Подгрузка значений доп. свойств
-        if (!is_null($this->properties)) {
-            $PropertyValues = [];
-
-            foreach ($this->properties as $Property) {
-                $propValueTable = 'Property_' . ucfirst($Property->type());
-                $Values = Core::factory($propValueTable)
-                    ->queryBuilder()
-                    ->where('model_name', '=', 'Lid')
-                    ->where('property_id', '=', $Property->getId())
-                    ->whereIn('object_id', $this->foundObjectsIds)
-                    ->orderBy('object_id', 'DESC')
-                    ->findAll();
-
-                $PropertyValues = array_merge($PropertyValues, $Values);
-            }
-
-            foreach ($Lids as $Lid) {
-                foreach ($PropertyValues as $Value) {
-                    if ($Lid->getId() == $Value->objectId()) {
-                        $Lid->addEntity($Value, 'property_value');
-                    }
-                }
-            }
-        }
+        $this->addPropValues($Lids);
 
         //Поиск комментариев
         if ($this->isWithComments === true) {
@@ -269,16 +297,29 @@ class Lid_Controller_Extended extends Controller
             ?   $OutputXml->addSimpleEntity('buttons-panel', '1')
             :   $OutputXml->addSimpleEntity('buttons-panel', '0');
 
+        //Формирование списка приоритетов
+        $priorities = [];
+        $priorities[0] = new stdClass();
+        $priorities[0]->id = 1;
+        $priorities[0]->title = 'Низкий';
+        $priorities[1] = new stdClass();
+        $priorities[1]->id = 2;
+        $priorities[1]->title = 'Средний';
+        $priorities[2] = new stdClass();
+        $priorities[2]->id = 3;
+        $priorities[2]->title = 'Выысокий';
+
         $OutputXml
             ->addEntities($this->getLids())
             ->addEntities(Core::factory('Schedule_Area')->getList())
             ->addEntities(Core::factory('Lid_Status')->getList())
+            ->addEntities($priorities, 'lid_priority')
             ->addEntities(Lid_Status::getColors(), 'color')
             ->addSimpleEntity('access_lid_create', (int)Core_Access::instance()->hasCapability(Core_Access::LID_CREATE))
             ->addSimpleEntity('access_lid_edit', (int)Core_Access::instance()->hasCapability(Core_Access::LID_EDIT))
             ->addSimpleEntity('access_lid_comment', (int)Core_Access::instance()->hasCapability(Core_Access::LID_APPEND_COMMENT));
 
-        return parent::show($OutputXml);
+        return parent::show($OutputXml)->show();
     }
 
 }
