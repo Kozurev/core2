caf79f9faf91c1034d8cc988d6665998540cd5cd
Egor <creative27016@gmail.com>
2021-01-11 02:17:47 +0300

    Удален вывод ошибок

--- includes.php
+++ includes.php
@@ -47,7 +47,7 @@ register_shutdown_function(function() {
     if (!is_null($error)) {
         $errorLogMessage = 'Error in file ' . $error['file'] . ' on line ' . $error['line'] . ':' . PHP_EOL . $error['message'] . PHP_EOL;
         Log::instance()->error(Log::TYPE_CORE, $errorLogMessage);
-dd($errorLogMessage);
+
         try {
             $mail = \Model\Mail::factory();
             $mail->addAddress(ADMIN_EMAIL);
